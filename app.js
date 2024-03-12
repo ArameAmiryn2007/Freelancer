658869f985af2660d0990edd96ce40039acd8508
ArameAmiryn2007 <145911375+ArameAmiryn2007@users.noreply.github.com>
2024-03-12 16:16:44 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-AOS.init();
+document.getElementById("ion2").addEventListener("click",function(){
+    document.getElementById("img2").style = "display:none"
+    document.getElementById("img1").style = "display:block"
+})
+document.getElementById("ion").addEventListener("click",function(){
+    document.getElementById("img2").style = "display:block"
+    document.getElementById("img1").style = "display:none"
+    
+})
+document.getElementById("pages2").addEventListener("click",function(){
+    document.getElementById("klk2").style = "display:block"
+})
+document.getElementById("mm3").addEventListener("click",function(){
+    document.getElementById("klk2").style = "display:none"
+})
